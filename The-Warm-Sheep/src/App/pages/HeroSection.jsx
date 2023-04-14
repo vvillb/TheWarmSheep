35d488e1f948d625c03befe123d404b3e1687b90
Virginia Villalobos <virginiavt98@gmail.com>
2023-04-14 09:24:34 +0200

    still trying

--- The-Warm-Sheep/src/App/pages/HeroSection.jsx
+++ The-Warm-Sheep/src/App/pages/HeroSection.jsx
@@ -1,42 +1,54 @@
 import React from 'react'
 import '../../styles.css'
-import { Box, Card, CardMedia, Paper, Typography } from '@mui/material'
-import playa from '../../assets/images/playa.jpg'
+import { Box, Card, CardMedia, Grid, Paper, Typography } from '@mui/material'
+import playa from '../../assets/images/playa1.jpg'
 
 
 const HeroSection = () => {
   return (
-  <Box>
-          
-            <Card
+  <Grid container>
+      <Grid item
             sx={{
-                position: 'absolute',
+              
+                position:'absolute',
+                // top:0,
+                // left:0,
+                // width:'100%',
+                height:'100%',   
+                display: 'flex',
+                alignItems:'center',
+                justifyContent:'center' ,
                 left: '238px',
-                top: '187px',
+                top: '0',
                 width: '1102px',
-                height: '800px',
+                objectFit:'cover',
+                // transform: 'translate(-50%, -50%)',
+                //  maxWidth: '100%',
+                // height: 'auto',
+
                 }}  
                 >
                 <img
                     src={playa}
-                    sx={{ minHeight: '100vh', 
-                    maxWidth: '75%', 
-                    margin: '0 auto', 
-                    display: 'block' 
-                  }}
+                    
                 />
-          </Card>
-           
-          
+      </Grid>
+      <Grid item
+      sx={{
+        maxWidth: '100%',
+      }}>
           <Typography variant='h1' 
           sx={{
+              textShadow:'0px 0px 20px rgba(0,0,0,0.4)',
+              fontSize:'clamp(5rem,8vw,20rem)',
               textAlign: 'left',
               position: 'absolute',
               left: '80px',
               top: '231px',
               width: '470px',
               height: '80px',
-              flexDirection: 'column' }}>
+              flexDirection: 'column',
+              flexWrap:'wrap' }}>
               The Warm Sheep
           </Typography>
           <Typography variant='h3'
@@ -46,13 +58,14 @@ const HeroSection = () => {
               top: '610px',
               width: '470px',
               height: '80px',
-              flexDirection: 'column' 
+              flexDirection: 'column' ,
+              flexWrap:'wrap' ,
+
           }}>
               Handmade Knits
           </Typography>
-
-        
-          </Box>
+      </Grid>
+</Grid>
   )
 }
 
--- The-Warm-Sheep/src/App/pages/HomePage.jsx
+++ The-Warm-Sheep/src/App/pages/HomePage.jsx
@@ -11,19 +11,24 @@ const HomePage = () => {
   return (
     <React.Fragment >
     <AppLayout>
-      <HeroSection/>
-      <Box >
-       
-      <Grid container spacing={0}
-          direction="column"
-          alignItems="center"
-          justifyContent="center"
-          sx={{minHeight:'100vh',backgroundColor:'secondary.main'}}
-          
-          >  <Box>
-                 
-         
-          
+      <Grid container>
+        
+        <Grid item
+        sx={{
+          height:'100vh',
+          maxHeight:'1100px',
+          position:'relative',
+          // overflow:'hidden'
+        }}>
+        <HeroSection/>
+        </Grid>
+
+         <Grid item spacing={0}
+              direction="column"
+              alignItems="center"
+              justifyContent="center"
+              sx={{minHeight:'100vh',backgroundColor:'secondary.main'}}
+              >          
           <Typography variant='body1' >
             Minim minim adipisicing dolore ut deserunt nostrud. Ut enim nostrud et consectetur deserunt commodo mollit duis cillum aliqua ullamco anim. Amet eiusmod ex id culpa do eu deserunt anim occaecat esse. Qui ex cillum culpa enim laboris duis. Laborum non ullamco proident culpa sit duis. Mollit fugiat ex commodo duis sunt ea culpa.
           Excepteur sit nulla irure consequat esse incididunt dolor proident voluptate reprehenderit labore quis et tempor. Minim do non amet dolore quis nulla magna dolor dolor do adipisicing fugiat velit aliquip. Labore cupidatat mollit Lorem consectetur elit sunt do eu veniam tempor cupidatat Lorem in proident. Consectetur incididunt esse minim duis Lorem voluptate est amet. Lorem sunt ut aute sit amet duis sit esse deserunt quis ea non sint.
@@ -31,16 +36,10 @@ const HomePage = () => {
           Pariatur ex laboris dolore aliqua laboris nisi nostrud velit aliqua ipsum magna ad. Aliquip deserunt fugiat eu voluptate est aliquip duis ea anim id pariatur irure cillum. Aliquip velit nisi id est voluptate et aute laborum consequat eiusmod sunt dolor. Esse sit reprehenderit est sint sunt.
          aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
           </Typography>
+        </Grid>               
         
-          </Box>
-        
-        
-        
-        </Grid>  
-        </Box>
-     
-     
-    </AppLayout>
+     </Grid>
+    </AppLayout>    
     </React.Fragment>
   )
 }
