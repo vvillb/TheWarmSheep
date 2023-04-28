1c172fcb7e2aae5e1f30baaba55dedbbbd9daa86
Virginia Villalobos <virginiavt98@gmail.com>
2023-04-28 13:31:51 +0200

    product gallery

--- TWS/src/App/components/ProductGallery.jsx
+++ TWS/src/App/components/ProductGallery.jsx
@@ -1,30 +1,323 @@
 import { Box, ImageList, ImageListItem } from '@mui/material'
 import React from 'react'
+import setNaranja1 from  '../../assets/images/products/setNaranja1.jpg'
+import setNaranja2 from  '../../assets/images/products/setNaranja2.jpg'
+import balaclavaAzul1 from  '../../assets/images/products/balaclavaAzul1.jpg'
+import balaclavaAzul2  from  '../../assets/images/products/balaclavaAzul2.jpg'
+import balaclavaAzul3 from  '../../assets/images/products/balaclavaAzul3.jpg'
+import balaclavaRosa1 from  '../../assets/images/products/balaclavaRosa1.jpg'
+import  balaclavaRosa2 from  '../../assets/images/products/balaclavaRosa2.jpg'
+import  BalaclavaRosa3 from  '../../assets/images/products/BalaclavaRosa3.jpg'
+import beanieAzul from  '../../assets/images/products/beanieAzul.jpg'
+import BolsoVerde3 from  '../../assets/images/products/BolsoVerde3.jpg'
+import BolsoVerde4 from  '../../assets/images/products/BolsoVerde4.jpg'
+import BolsoVerde5 from  '../../assets/images/products/BolsoVerde5.jpg'
+import bufandaMarron1 from  '../../assets/images/products/bufandaMarron1.jpg'
+import bufandaMarron2 from  '../../assets/images/products/bufandaMarron2.jpg'
+import bufandaMarron3 from  '../../assets/images/products/bufandaMarron3.jpg'
+import bufandaMarron4 from  '../../assets/images/products/bufandaMarron4.jpg'
+import bufandaMarron5 from  '../../assets/images/products/bufandaMarron5.jpg'
+import bufandaMarron6 from  '../../assets/images/products/bufandaMarron6.jpg'
+import bufandaVerde2 from  '../../assets/images/products/bufandaVerd2.jpg'
+import bufandaVerde1 from  '../../assets/images/products/bufandaVerde1.jpg'
+import cuelloVErde from  '../../assets/images/products/cuelloVErde.jpg'
+import jerseyAzul1 from  '../../assets/images/products/jerseyAzul1.jpg'
+import jerseyAzul2 from  '../../assets/images/products/jerseyAzul2.jpg'
+import jerseyAzul3 from  '../../assets/images/products/jerseyAzul3.jpg'
+import jerseyAzul4 from  '../../assets/images/products/jerseyAzul4.jpg'
+import jerseyAzul5 from  '../../assets/images/products/jerseyAzul5.jpg'
+import jerseyAzul6 from  '../../assets/images/products/jerseyAzul6.jpg'
+import JerseyMangas1 from  '../../assets/images/products/JerseyMangas1.jpg'
+import JerseyMangas2 from  '../../assets/images/products/JerseyMangas2.jpg'
+import JerseyMangas3 from  '../../assets/images/products/JerseyMangas3.jpg'
+import JerseyMangas4 from  '../../assets/images/products/JerseyMangas4.jpg'
+import JerseyMangas5 from  '../../assets/images/products/JerseyMangas5.jpg'
+import jerseyMulticolor1 from  '../../assets/images/products/jerseyMulticolor1.jpg'
+import jerseyMulticolor2 from  '../../assets/images/products/jerseyMulticolor2.jpg'
+import jerseyVerde1 from  '../../assets/images/products/jerseyVerde1.jpg'
+import jerseyVerde2 from  '../../assets/images/products/jerseyVerde2.jpg'
+import mittensVerdes1 from  '../../assets/images/products/mittensVerdes1.jpg'
+import mittensVerdes2 from  '../../assets/images/products/mittensVerdes2.jpg'
 
 
 
-const itemData = [
-    {
-      id:1,
-      img: 'https://images.unsplash.com/photo-1549388604-817d15aa0110',
-      title: 'Bed',
-    },];
 
 
 const ProductGallery = () => {
   return (
-    <Box sx={{ width: 500, height: 450, overflowY: 'scroll' }}>
+    <Box sx={{ padding:'2%'}}>
     <ImageList variant="masonry" cols={3} gap={8}>
-      {itemData.map((item) => (
-        <ImageListItem key={item.id}>
+      
+        <ImageListItem >
           <img
-            src={item.img}
-            srcSet={item.img}
-            alt={item.title}
+            src={setNaranja1}
+            alt='Set Naranja'
             loading="lazy"
           />
         </ImageListItem>
-      ))}
+        <ImageListItem >
+          <img
+            src={setNaranja2}
+            alt='Set Naranja'
+            loading="lazy"
+          />
+        </ImageListItem>
+        <ImageListItem >
+          <img
+            src={balaclavaAzul1}
+            alt='Set Naranja'
+            loading="lazy"
+          />
+        </ImageListItem>
+        <ImageListItem >
+          <img
+            src={balaclavaAzul2}
+            alt='Set Naranja'
+            loading="lazy"
+          />
+        </ImageListItem>
+        <ImageListItem >
+          <img
+            src={balaclavaAzul3}
+            alt='Set Naranja'
+            loading="lazy"
+          />
+        </ImageListItem>
+        <ImageListItem >
+          <img
+            src={balaclavaRosa1}
+            alt='Set Naranja'
+            loading="lazy"
+          />
+        </ImageListItem>
+        <ImageListItem >
+          <img
+            src={balaclavaRosa2}
+            alt='Set Naranja'
+            loading="lazy"
+          />
+        </ImageListItem>
+        <ImageListItem >
+          <img
+            src={BalaclavaRosa3}
+            alt='Set Naranja'
+            loading="lazy"
+          />
+        </ImageListItem>
+        <ImageListItem >
+          <img
+            src={beanieAzul}
+            alt='Set Naranja'
+            loading="lazy"
+          />
+        </ImageListItem>
+        <ImageListItem >
+          <img
+            src={BolsoVerde3}
+            alt='Set Naranja'
+            loading="lazy"
+          />
+        </ImageListItem>
+        <ImageListItem >
+          <img
+            src={BolsoVerde4}
+            alt='Set Naranja'
+            loading="lazy"
+          />
+        </ImageListItem>
+        <ImageListItem >
+          <img
+            src={BolsoVerde5}
+            alt='Set Naranja'
+            loading="lazy"
+          />
+        </ImageListItem>
+        <ImageListItem >
+          <img
+            src={bufandaMarron1}
+            alt='Set Naranja'
+            loading="lazy"
+          />
+        </ImageListItem>
+        <ImageListItem >
+          <img
+            src={bufandaMarron2}
+            alt='Set Naranja'
+            loading="lazy"
+          />
+        </ImageListItem>
+        <ImageListItem >
+          <img
+            src={bufandaMarron3}
+            alt='Set Naranja'
+            loading="lazy"
+          />
+        </ImageListItem>
+        <ImageListItem >
+          <img
+            src={bufandaMarron4}
+            alt='Set Naranja'
+            loading="lazy"
+          />
+        </ImageListItem>
+        <ImageListItem >
+          <img
+            src={bufandaMarron5}
+            alt='Set Naranja'
+            loading="lazy"
+          />
+        </ImageListItem>
+        <ImageListItem >
+          <img
+            src={bufandaMarron6}
+            alt='Set Naranja'
+            loading="lazy"
+          />
+        </ImageListItem>
+        <ImageListItem >
+          <img
+            src={bufandaVerde2}
+            alt='Set Naranja'
+            loading="lazy"
+          />
+        </ImageListItem>
+        <ImageListItem >
+          <img
+            src={bufandaVerde1}
+            alt='Set Naranja'
+            loading="lazy"
+          />
+        </ImageListItem>
+        <ImageListItem >
+          <img
+            src={cuelloVErde}
+            alt='Set Naranja'
+            loading="lazy"
+          />
+        </ImageListItem>
+        <ImageListItem >
+          <img
+            src={jerseyAzul1}
+            alt='Set Naranja'
+            loading="lazy"
+          />
+        </ImageListItem>
+        <ImageListItem >
+          <img
+            src={jerseyAzul2}
+            alt='Set Naranja'
+            loading="lazy"
+          />
+        </ImageListItem>
+        <ImageListItem >
+          <img
+            src={jerseyAzul3}
+            alt='Set Naranja'
+            loading="lazy"
+          />
+        </ImageListItem>
+        <ImageListItem >
+          <img
+            src={jerseyAzul4}
+            alt='Set Naranja'
+            loading="lazy"
+          />
+        </ImageListItem>
+        <ImageListItem >
+          <img
+            src={jerseyAzul5}
+            alt='Set Naranja'
+            loading="lazy"
+          />
+        </ImageListItem>
+        <ImageListItem >
+          <img
+            src={jerseyAzul6}
+            alt='Set Naranja'
+            loading="lazy"
+          />
+        </ImageListItem>
+        <ImageListItem >
+          <img
+            src={JerseyMangas1}
+            alt='Set Naranja'
+            loading="lazy"
+          />
+        </ImageListItem>
+        <ImageListItem >
+          <img
+            src={JerseyMangas2}
+            alt='Set Naranja'
+            loading="lazy"
+          />
+        </ImageListItem>
+        <ImageListItem >
+          <img
+            src={JerseyMangas3}
+            alt='Set Naranja'
+            loading="lazy"
+          />
+        </ImageListItem>
+        <ImageListItem >
+          <img
+            src={JerseyMangas4}
+            alt='Set Naranja'
+            loading="lazy"
+          />
+        </ImageListItem>
+        <ImageListItem >
+          <img
+            src={JerseyMangas5}
+            alt='Set Naranja'
+            loading="lazy"
+          />
+        </ImageListItem>
+        <ImageListItem >
+          <img
+            src={jerseyMulticolor1}
+            alt='Set Naranja'
+            loading="lazy"
+          />
+        </ImageListItem>
+        <ImageListItem >
+          <img
+            src={jerseyMulticolor2}
+            alt='Set Naranja'
+            loading="lazy"
+          />
+        </ImageListItem>
+        <ImageListItem >
+          <img
+            src={jerseyVerde1}
+            alt='Set Naranja'
+            loading="lazy"
+          />
+        </ImageListItem>
+        <ImageListItem >
+          <img
+            src={jerseyVerde2}
+            alt='Set Naranja'
+            loading="lazy"
+          />
+        </ImageListItem>
+        <ImageListItem >
+          <img
+            src={mittensVerdes1}
+            alt='Set Naranja'
+            loading="lazy"
+          />
+        </ImageListItem>
+        <ImageListItem >
+          <img
+            src={mittensVerdes2}
+            alt='Set Naranja'
+            loading="lazy"
+          />
+        </ImageListItem>
+      
+
+
+      
     </ImageList>
   </Box>
   );
--- TWS/src/App/pages/ProductPage.jsx
+++ TWS/src/App/pages/ProductPage.jsx
@@ -6,18 +6,13 @@ import SingleProduct from '../components/SingleProduct'
 import foto1 from '../../assets/images/products/product1.jpg'
 import foto2 from '../../assets/images/products/product2.jpg'
 import fondo from '../../assets/images/playa.jpg'
+import ProductGallery from '../components/ProductGallery'
 
 
 const ProductPage = () => {
   return (
     <AppLayout>
-        <Typography variant='h2'>
-            Algunas creaciones...
-        </Typography>
-
-        <SingleProduct fondo={fondo} titulo="jhgdh" foto1={foto1} foto2={foto2}>
-            Lana ...Consectetur dolor adipisicing adipisicing nisi commodo ullamco ea. Et ullamco do reprehenderit occaecat anim. Aliqua do irure duis non nostrud dolore laboris Lorem deserunt mollit enim. Duis incididunt aute laborum excepteur ullamco esse irure velit cillum aliqua adipisicing sunt proident.
-        </SingleProduct>
+        <ProductGallery/>
 
       <Footer/>
     </AppLayout>
